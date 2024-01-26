74cd7b438d1839dec2210d250df13dbe5325e60c
terfefed <133997569+terfefed@users.noreply.github.com>
2024-01-26 19:56:26 +0530

    Time: 10 ms (42.77%), Space: 11 MB (22.49%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         const int MOD = 1e9 + 7;
-        unordered_map<long long, int> memo;
 
-        return dfs(m, n, maxMove, startRow, startColumn, memo, MOD);
-    }
+        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<vector<int>> temp(m, vector<int>(n, 0));
 
-private:
-    int dfs(int m, int n, int maxMove, int i, int j, unordered_map<long long, int>& memo, const int MOD) {
-        if (i < 0 || i >= m || j < 0 || j >= n) {
-            return 1;  // Out of boundary
-        }
-
-        if (maxMove == 0) {
-            return 0;  // No more moves allowed
-        }
-
-        long long key = ((long long)i << 32) | ((long long)j << 16) | maxMove;
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
-        }
-
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        dp[startRow][startColumn] = 1;
         int result = 0;
 
-        for (const auto& direction : directions) {
-            int ni = i + direction.first;
-            int nj = j + direction.second;
+        for (int move = 1; move <= maxMove; ++move) {
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (dp[i][j] > 0) {
+                        for (const auto& direction : directions) {
+                            int ni = i + direction[0];
+                            int nj = j + direction[1];
 
-            result = (result + dfs(m, n, maxMove - 1, ni, nj, memo, MOD)) % MOD;
+                            if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                                temp[ni][nj] = (temp[ni][nj] + dp[i][j]) % MOD;
+                            } else {
+                                result = (result + dp[i][j]) % MOD;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            dp = temp;
+            temp = vector<vector<int>>(m, vector<int>(n, 0));
         }
 
-        memo[key] = result;
         return result;
     }
 };
