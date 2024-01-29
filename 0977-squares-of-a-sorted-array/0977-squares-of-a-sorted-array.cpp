ba985d7d63dca1875a7570cf421b8df45f2aa127
terfefed <133997569+terfefed@users.noreply.github.com>
2024-01-29 21:18:48 +0530

    Time: 15 ms (96.74%), Space: 30.6 MB (5.17%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> result;
         for(int i=0;i<nums.size();i++)
         {
-            result.push_back(nums[i]*nums[i]);
+            result.push_back(pow(nums[i],2));
         }
         sort(result.begin(),result.end());
         return result;
