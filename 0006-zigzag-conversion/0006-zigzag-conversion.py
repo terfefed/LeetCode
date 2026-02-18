6c0dd90bdc92432c543275a51b68378474d8b1e4
Allen Joseph <alljoe947@gmail.com>
2026-02-18 19:08:36 +0530

    Time: 4 ms (95.21%), Space: 19.4 MB (39.48%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,16 +2,16 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1 or numRows >= len(s):
             return s
-        res=""
-        lis = [[""] * len(s) for _ in range(numRows)]
-        x=0
-        direction=1
-        for i in range(len(s)):
-            lis[x][i]=s[i]
-            x+=direction
-            if x == numRows-1: direction = -1
-            if x ==0 : direction =1
-        for i in range(numRows):
-            res+="".join(lis[i])
-        return res
-
+        
+        rows = [""] * numRows
+        curRow = 0
+        direction = 1
+        
+        for ch in s:
+            rows[curRow] += ch
+            curRow += direction
+            
+            if curRow == 0 or curRow == numRows - 1:
+                direction *= -1
+        
+        return "".join(rows)
